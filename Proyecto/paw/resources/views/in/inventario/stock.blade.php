f611073f1e6bb51e6c7d0a6f68297fa7824911ba
fcataniae <cataniafrane@gmail.com>
2019-06-23 01:47:05 -0300

    agrega blade stock, cambia css menu y colores

---
+++ Proyecto/paw/resources/views/in/inventario/stock.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@section('head-css')
+	<link rel="stylesheet" href="{{asset('css/app.css')}}"/>
+@endsection
+
+@section('head-js')
+	<script src="{{asset('js/utils.js')}}"></script>
+@endsection
+@section('body-header')
+	<h1 class="logo"><a href="#">Logo Tienda</a></h1>
+	@include('partials.nav-principal')
+@endsection
+@section('body-main')
+	@include('partials.nav-lateral-negocio')
+	<section>
+		<p>Bienvenido al apartado de INVENTARIO {{auth()->user()->name }}</p>
+	</section>
+@endsection
+@section('body-footer')
+	<address>Guerrero, Pedro</address>
+	<address>Telefono: 11235687</address>
+@endsection
