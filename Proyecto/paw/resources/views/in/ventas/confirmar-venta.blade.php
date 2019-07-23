7a1c0bca9611c4471572c3a930c81f0be1c0294a
Pebros <peg10@live.com.ar>
2019-07-22 23:02:52 -0300

    Se agrego css para confirmar factura

--- Proyecto/paw/resources/views/in/ventas/confirmar-venta.blade.php
+++ Proyecto/paw/resources/views/in/ventas/confirmar-venta.blade.php
@@ -26,54 +26,61 @@
 			{{ csrf_field() }}
 			<fieldset name="Total">
 				<legend>Factura</legend>
-				<label for="id">Nro Factura: </label>
-				<input type="number" id="id" name="id" min="0" class="input" readonly value= "{{ $factura->id }}">
-				<label for="total">Total ($): </label>
-				<input type="number" id="total" name="total" min="0" class="input" readonly value= "{{ $factura->importe }}">
+				<div class="group-inline">
+					<label for="id">Nro Factura: </label>
+					<input type="number" id="id" name="id" min="0" class="input" readonly value= "{{ $factura->id }}">
+				</div>
+				<div class="group-inline">
+					<label for="total">Total ($): </label>
+					<input type="number" id="total" name="total" min="0" class="input" readonly value= "{{ $factura->importe }}">
+				</div>
 			</fieldset>
 			<br>
 			<div id="msjInfo"></div>
 			<fieldset name="Buscador">
 				<legend>Cliente</legend>
-				<label for="es_cliente">Es cliente? </label>
-				<select onChange="determinarCliente()" id="es_cliente" name="es_cliente" class="input">
-				    <option id="1" value="NO" selected>NO</option>
-				    <option id="2" value="SI">SI</option>
-				</select>
-				<div id="datos_cliente" style="display:none">
+				<div class="group-inline">
+					<label for="es_cliente">Es cliente? </label>
+					<select onChange="determinarCliente()" id="es_cliente" name="es_cliente" class="input">
+					    <option id="1" value="NO" selected>NO</option>
+					    <option id="2" value="SI">SI</option>
+					</select>
+				</div>
+				<div class="group-inline" id="datos_cliente" style="display:none">
 					<label for="nro_cliente"> Nro Cliente: </label>
 					<input type="text" id="nro_cliente" name="nro_cliente" list="clientes_data" onChange="cargarCliente()" class="input">
 					<datalist id="clientes_data"></datalist>
 				</div>
 				<br>
 				<br>
-				<label for="nro_documento">Documento: </label>
-				<select id="tipo_documento" name="tipo_documento" class="input">
-				    <option id="1" value="DNI">DNI</option>
-				    <option id="2" value="CUIL">CUIL</option>
-				</select>
-				<input type="number" id="nro_documento" name="nro_documento" min="0" class="input">
-				<label for="nombre"> Nombre: </label>
-				<input type="text" id="nombre" name="nombre" class="input">
-				<label for="apellido"> Apellido: </label>
-				<input type="text" id="apellido" name="apellido" class="input">
+				<div class="group-inline">
+					<label for="nro_documento">Documento: </label>
+					<select id="tipo_documento" name="tipo_documento" class="input">
+					    <option id="1" value="DNI">DNI</option>
+					    <option id="2" value="CUIL">CUIL</option>
+					</select>
+					<input type="number" id="nro_documento" name="nro_documento" min="0" class="input">
+				</div>
+				<div class="group-inline">
+					<label for="nombre"> Nombre: </label>
+					<input type="text" id="nombre" name="nombre" class="input">
+				</div>
+				<div class="group-inline">
+					<label for="apellido"> Apellido: </label>
+					<input type="text" id="apellido" name="apellido" class="input">
+				</div>
 				<button id="btnAddCliente" type="button" onClick="agregarCliente()" class="button-table"><i class="fa fa-plus" aria-hidden="true"></i></button>
 			</fieldset>
 			<br>
 			<fieldset name="Detalles">
 				<legend>Forma de Pago</legend>
-				<div id="formulario"></div>
-				<label for="forma_pago">Forma de Pago: </label>
-				<select id="forma_pago" name="forma_pago" class="input">
-				    <option id="1" value="1" selected>EFECTIVO</option>
-				    <option id="2" value="2">TARJETA DEBIDO</option>
-				    <option id="3" value="3">TARJETA CREDITO</option>
-				</select>
-				<div id="debito">
-					
-				</div>
-				<div id="credito">
-					
+				<div class="group-inline">
+					<label for="forma_pago">Forma de Pago: </label>
+					<select id="forma_pago" name="forma_pago" class="input">
+					    <option id="1" value="1" selected>EFECTIVO</option>
+					    <option id="2" value="2">TARJETA DEBIDO</option>
+					    <option id="3" value="3">TARJETA CREDITO</option>
+					</select>
 				</div>
 			</fieldset>
 			<br>
