e8b4f01245e8595c9810bbb8358c5e43cde37148
Pebros <peg10@live.com.ar>
2020-07-06 00:34:53 -0300

    Se ajusto nav-principal y css relacionado al mismo

--- Proyecto/paw/resources/views/partials/nav-principal.blade.php
+++ Proyecto/paw/resources/views/partials/nav-principal.blade.php
@@ -1,33 +1,36 @@
 @if(Auth::user() != null)
-	<a class="logo" href={{ route('in') }}>
-		<img src="/img/logo.png" alt="" width="40" height="40"/>
+	<a href={{ route('in') }}>
+		<img class="logo" src="/img/logo.png" alt="" width="40" height="40"/>
 	</a>
 @endif
 
 <nav>
 	<ul class="main-nav">
 		@if(Entrust::can('gestionar_negocio'))
-			<li class="negocio"><a href="#">Negocio</a></li>
+			<li class="negocio menu"><a href="#">Negocio</a></li>
 	    @endif
 
 	    @if(Entrust::can('gestionar_venta'))
-		    <li class="venta"><a href="#">Ventas</a></li>
+		    <li class="venta menu"><a href="#">Ventas</a></li>
 	    @endif
 
 	    @if(Entrust::can('gestionar_inventario'))
-		    <li class="inventario"><a href="#">Inventario</a></li>
+		    <li class="inventario menu"><a href="#">Inventario</a></li>
 	    @endif
 
 	    @if(Entrust::can('gestionar_reporte'))
-		    <li class="reporte"><a href="{{ route('in.reportes')}}">Reporte</a></li>
+		    <li class="reporte menu"><a href="{{ route('in.reportes')}}">Reporte</a></li>
 	    @endif
 
-	    <li><a><strong>{{ Auth::user()->name }}</strong></a>
-	        <ul>
-	        	<li><a href={{ route('in.users.edit.datospersonal') }}>Datos Personales</a></li>
-	            <li><a href={{ route('in.users.edit.datoscuenta') }}>Configurar Cuenta</a></li>
-	            <li><a href="{{ route('auth.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-    Logout</a></li>
+	    <li class="perfil menu">
+	    	<a>
+	    		<img class="avatar" src="/img/fotoPerfil.jpg" alt=""/>
+	    		<strong>{{ Auth::user()->name }}</strong>
+	    	</a>
+	        <ul class="submenu">
+	        	<li class="submenu1"><a href={{ route('in.users.edit.datospersonal') }}>Datos Personales</a></li>
+	            <li class="submenu2"><a href={{ route('in.users.edit.datoscuenta') }}>Configurar Cuenta</a></li>
+	            <li class="submenu3"><a href="{{ route('auth.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
 	        </ul>
 	    </li>
 	</ul>
