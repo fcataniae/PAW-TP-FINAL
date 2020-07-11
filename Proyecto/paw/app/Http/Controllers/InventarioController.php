b1bbecdd7794f55f788bdc86b0f4ecd0ae78d1fa
Franco Catania <frcatania@santandertecnologia.com.ar>
2020-07-10 23:56:31 -0300

    cambias InventarioController

--- Proyecto/paw/app/Http/Controllers/InventarioController.php
+++ Proyecto/paw/app/Http/Controllers/InventarioController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Auth;
-use ProductosController;
 
 class InventarioController extends Controller
 {
+    protected $controller;
+    
+    public function __construct(){
+      $this->controller = new ProductosController();
+    }
+
     public function index(){
+
     	if(Auth::user()->can('gestionar_inventario')){
     		return view('in.inventario.index');
     	}else{
@@ -18,9 +24,10 @@ class InventarioController extends Controller
     }
 
     public function stock(){
+
       if(Auth::user()->can('gestionar_inventario')){
 
-        $data = app('App\Http\Controllers\ProductosController')->showAll();
+        $data = $this->controller->showAll();
 
         return view('in.inventario.stock')
           ->with('columnas', json_encode($data['columnas']))
@@ -32,13 +39,14 @@ class InventarioController extends Controller
 
     public function update(Request $request){
       if(Auth::user()->can('gestionar_inventario')){
+
         $this->validate($request,[
                'id' => 'required',
                'stock' => 'required|numeric|min:0',
                'comentario' => 'required',
            ]);
 
-        $act = app('App\Http\Controllers\ProductosController')->updateStock($request->post('id'),$request->post('stock'),$request->post('comentario'),Auth::user()->name);
+        $act = $this->controller->updateStock($request->post('id'),$request->post('stock'),$request->post('comentario'),Auth::user()->name);
         if($act){
           return redirect()->route('in.inventario.stock');
         }else {
@@ -51,7 +59,8 @@ class InventarioController extends Controller
 
     public function reposicion(){
       if(Auth::user()->can('gestionar_inventario')){
-        $prod = app('App\Http\Controllers\ProductosController')->findById(Input::get('id'));
+
+        $prod =$this->controller->findById(Input::get('id'));
         $producto = json_decode($prod,true);
         return view('in.inventario.reposicion')->with(['data'=>$producto]);
       }else{
