a8bc3d8d2f1fec744ace1cf6163ed90dfdfcb271
fcataniae <cataniafrane@gmail.com>
2020-10-17 00:06:31 -0300

    mueve scripts de html a .js

--- Proyecto/paw/public/js/reportes.js
+++ Proyecto/paw/public/js/reportes.js
@@ -2,105 +2,30 @@ var window = window || {},
     document = document || {},
     console = console || {};
 
-document.addEventListener("DOMContentLoaded", function () {
-  initWindow();
-  ajaxCall('GET','/in/forma_pago',completeFormaPago);
-  ajaxCall('GET','/in/empleado',completeEmpleado);
-  ajaxCall('GET','/in/cliente',completeCliente);
-});
 
+document.addEventListener("DOMContentLoaded", () => {
+  filters = filtros;
+  filterUrl = '/in/factura';
+  drawFilters();
 
-function initWindow(){
-
-  let input = document.querySelector('section.main input.button-clean');
-  input.addEventListener('click', doSearch);
-}
-
-function doSearch(){
-  let params = [];
-  let filters = document.querySelectorAll('input.form-input.minified');
-  filters.forEach( filter => {
-    let value = filter.value;
-    if(value && value.trim() != ''){
-      let param = {
-        'query' : filter.id,
-        'value' : value
-      };
-      params.push(param);
-  }
-  });
   
-  ajaxCallWParameters('GET','/in/factura',params,printTable,showError);
-}
-
-
-function printTable(res){
-  res = JSON.parse(res);
-  destroyTabla();
-  addJsHandler(res);
-  encabezados = res.columnas;
-  registrosFiltrados = res.registros;
-  registros = res.registros;
-  construirTabla(encabezados, registros);
-  paginarAndVisualizarRegistros(REGISTROS_POR_PAGINA, PAGINA_INICIAL);
-}
-
-function addJsHandler(res){
-  res.columnas.push({headerName: 'Detalle', field: 'accion', width: '10%'});
-  res.registros.forEach(res => {
-    nroFac = res.id;
-    res['action'] = { js: true, content: () => showDetails(nroFac) };
-  });
-}
-
-function showError(res,stat){
-  console.log(res);
-  console.log(stat);
-}
-
-function completeDatalist(res, datalist){
-  let data = JSON.parse(res);
-  let dlist = document.querySelector('datalist#' + datalist);
-  let option;
-  data.forEach(d => {
-    option = document.createElement('option');
-    option.value = d.id;
-    option.innerHTML = d.descripcion;
-    dlist.appendChild(option);
-  });
-}
-function completeEmpleado(res){
-
- completeDatalist(res, 'empleados_data');
-}
-
-function completeFormaPago(res){
-
-  completeDatalist(res, 'forma_pago_data');
-}
-
-function completeCliente(res){
-
-  completeDatalist(res, 'cliente_data');
-}
-
-async function showDetails(nroFac){
-
-  let response = await fetch(`/in/factura/get/detalles/${nroFac}`);
-  let detalles = await response.json();
-  let table,thead,tbody,tr,td,th;
-  const headers = ['producto','codigo','talle','precio','cantidad'];
-  table = document.createElement('table');
-  table.classList.add('table');
-  tbody = document.createElement('tbody');
-  thead = document.createElement('thead');
-  for(let i = 0; i< headers.length ;i++){
+  async function showDetails(nroFac){
+    
+    let response = await fetch(`/in/factura/get/detalles/${nroFac}`);
+    let detalles = await response.json();
+    let table,thead,tbody,tr,td,th;
+    const headers = ['producto','codigo','talle','precio','cantidad'];
+    table = document.createElement('table');
+    table.classList.add('table');
+    tbody = document.createElement('tbody');
+    thead = document.createElement('thead');
+    for(let i = 0; i< headers.length ;i++){
     th = document.createElement('th');
     th.innerHTML = headers[i];
     thead.appendChild(th);
-  }
-  table.appendChild(thead);
-  for(let i = 0; i< detalles.length ;i++){
+    }
+    table.appendChild(thead);
+    for(let i = 0; i< detalles.length ;i++){
     tr = document.createElement('tr');
     td = document.createElement('td');
     td.innerHTML = detalles[i].producto;
@@ -118,9 +43,42 @@ async function showDetails(nroFac){
     td.innerHTML = detalles[i].cantidad;
     tr.appendChild(td);
     tbody.appendChild(tr);
+    }
+    table.appendChild(tbody);
+    let container = document.createElement('div');
+    container.appendChild(table);
+    return container.outerHTML;
   }
-  table.appendChild(tbody);
-  let container = document.createElement('div');
-  container.appendChild(table);
-  return container.outerHTML;
-}
+
+  function getDetails(nroFac){
+    return () => {
+      return showDetails(nroFac);
+    }
+  }
+  
+  function addJsHandler(res){
+    res.columnas.push({headerName: 'Detalle', field: 'accion', width: '10%'});
+    res.registros.forEach(res => {
+      nroFac = res.id;
+      res['action'] = { js: true, content: getDetails(nroFac) };
+    });
+  }
+
+  callbackFn = function printTable(res){
+    res = JSON.parse(res);
+    destroyTabla();
+    addJsHandler(res);
+    encabezados = res.columnas;
+    registrosFiltrados = res.registros;
+    registros = res.registros;
+    construirTabla(encabezados, registros);
+    paginarAndVisualizarRegistros(REGISTROS_POR_PAGINA, PAGINA_INICIAL);
+  }
+
+  errorCalbackFn = function showError(res,stat){
+    console.log(res);
+    console.log(stat);
+  }
+});
+
+
--- Proyecto/paw/resources/views/in/reportes/reporte.blade.php
+++ Proyecto/paw/resources/views/in/reportes/reporte.blade.php
@@ -8,6 +8,7 @@
 
 @section('head-js')
 	<script src="{{asset('js/filtros.js')}}"></script>
+	<script src="{{asset('js/reportes.js')}}"></script>
 	<script src="{{asset('js/tabla.js')}}"></script>
 	<script src="{{asset('js/ajax.js')}}"></script>
 	<script src="{{asset('js/utils.js')}}"></script>
@@ -15,84 +16,6 @@
 		var columnas;
 		var datos;
 		var filtros = '{!! $filtros !!}';
-		document.addEventListener("DOMContentLoaded", () => {
-			filters = filtros;
-			filterUrl = '/in/factura';
-			drawFilters();
-
-			
-			async function showDetails(nroFac){
-				
-				let response = await fetch(`/in/factura/get/detalles/${nroFac}`);
-				let detalles = await response.json();
-				let table,thead,tbody,tr,td,th;
-				const headers = ['producto','codigo','talle','precio','cantidad'];
-				table = document.createElement('table');
-				table.classList.add('table');
-				tbody = document.createElement('tbody');
-				thead = document.createElement('thead');
-				for(let i = 0; i< headers.length ;i++){
-				th = document.createElement('th');
-				th.innerHTML = headers[i];
-				thead.appendChild(th);
-				}
-				table.appendChild(thead);
-				for(let i = 0; i< detalles.length ;i++){
-				tr = document.createElement('tr');
-				td = document.createElement('td');
-				td.innerHTML = detalles[i].producto;
-				tr.appendChild(td);
-				td = document.createElement('td');
-				td.innerHTML = detalles[i].codigo;
-				tr.appendChild(td);
-				td = document.createElement('td');
-				td.innerHTML = detalles[i].talle;
-				tr.appendChild(td);
-				td = document.createElement('td');
-				td.innerHTML = detalles[i].precio;
-				tr.appendChild(td);
-				td = document.createElement('td');
-				td.innerHTML = detalles[i].cantidad;
-				tr.appendChild(td);
-				tbody.appendChild(tr);
-				}
-				table.appendChild(tbody);
-				let container = document.createElement('div');
-				container.appendChild(table);
-				return container.outerHTML;
-			}
-
-			function getDetails(nroFac){
-				return () => {
-					return showDetails(nroFac);
-				}
-			}
-			
-			function addJsHandler(res){
-				res.columnas.push({headerName: 'Detalle', field: 'accion', width: '10%'});
-				res.registros.forEach(res => {
-					nroFac = res.id;
-					res['action'] = { js: true, content: getDetails(nroFac) };
-				});
-			}
-
-			callbackFn = function printTable(res){
-				res = JSON.parse(res);
-				destroyTabla();
-				addJsHandler(res);
-				encabezados = res.columnas;
-				registrosFiltrados = res.registros;
-				registros = res.registros;
-				construirTabla(encabezados, registros);
-				paginarAndVisualizarRegistros(REGISTROS_POR_PAGINA, PAGINA_INICIAL);
-			}
-
-			errorCalbackFn = function showError(res,stat){
-				console.log(res);
-				console.log(stat);
-			}
-		})
-
 	</script>
 	<style>
 		table{
